8c77264904aeadad91ab025d10f0ae14171db40d
Robert3141 <robert.aries1@gmail.com>
2020-10-28 14:55:56 +0000

    cosmocannons:fix weird turret bug

--- Cosmocannons/flutter_service_worker.js
+++ Cosmocannons/flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "fonts/AstroSpace.ttf": "5892adccd6c4b8c9e80f005e7eef06f2",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "29c83c5d5dc792ef623719dd2c1faa8d",
-"/": "29c83c5d5dc792ef623719dd2c1faa8d",
-"main.dart.js": "2a05f17bf44d886fbb7aa8032e8d40a1",
+"index.html": "1a73334cd9adb703569500293c3d7258",
+"/": "1a73334cd9adb703569500293c3d7258",
+"main.dart.js": "d33581d563af257f1bece03c43c55a20",
 "manifest.json": "eb9fa9a395ddef3e0705adc02e368a87",
 "style.css": "d92f7af692863290a67a9f4fcc3b1f38",
 "version.json": "90488955e0931f8914c37a1dd5d14a5b"
